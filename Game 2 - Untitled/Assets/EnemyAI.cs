a0dce02a953f3a7a08f4ee9f2c0b08c3e40bf1c8
Shadow-nm <Maitland.Nathaniel@gmail.com>
2026-02-27 18:21:25 -0600

    Enemy AI Improvements
    Enemy now has awareness of the player and will chase once close. If the player moves to the other side of the enemy the sprite will turn around.

--- Game 2 - Untitled/Assets/EnemyAI.cs
+++ Game 2 - Untitled/Assets/EnemyAI.cs
@@ -1,25 +1,76 @@
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
     public GameObject player;
-    public float speed;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _rotationSpeed;
+
+    private Rigidbody2D _rigidbody;
+    private PlayerAwarenessScript _playerAwarenessController;
+    private Vector2 _targetDirection;
+    
     public float AggroDist;
     public float AggroKeepDist;
 
     private float distance;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    
+    void Awake()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _playerAwarenessController = GetComponent<PlayerAwarenessScript>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        distance = Vector2.Distance(transform.position, player.transform.position);
-        Vector2 direction = player.transform.position - transform.position;
+        UpdateTargetDirection();
+        RotateTowardsTarget();
+        SetVelocity();
+        //distance = Vector2.Distance(transform.position, player.transform.position);
+        //Vector2 direction = player.transform.position - transform.position;
 
-        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        //transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+    }
+    private void UpdateTargetDirection()
+    {
+        if (_playerAwarenessController.AwareOfPlayer)
+        {
+            _targetDirection = _playerAwarenessController.DirectionToPlayer;
+        }
+        else
+        {
+            _targetDirection = Vector2.zero;
+        }
+    }
+
+    private void RotateTowardsTarget()
+    {
+        if (_targetDirection == Vector2.zero)
+        {
+            return;
+        }
+        else if (player.transform.position.x - transform.position.x > 0)
+        {
+            transform.localScale = new Vector2(-1, 1);
+        }
+        else if (player.transform.position.x - transform.position.x < 0)
+        {
+            transform.localScale = new Vector2(1, 1);
+        }
+
+    }
+
+    private void SetVelocity()
+    {
+        if (_targetDirection == Vector2.zero)
+        {
+            _rigidbody.linearVelocity = Vector2.zero;
+        }
+        else
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, _speed * Time.deltaTime);
+        }
     }
 }
