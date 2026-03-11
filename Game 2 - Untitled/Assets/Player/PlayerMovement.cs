0512b14211d376589fda5ec161814fed5d7a05db
Shadow-nm <Maitland.Nathaniel@gmail.com>
2026-03-11 08:09:24 -0500

    adds player animations and early enemy animations
    Adds all current player animations to player character besides climbing. Adds idle animation for an enemy character. Adds grounded detection for player character, removing infinite jumps. Enemy scaling is currently incorrect when chasing.

--- Game 2 - Untitled/Assets/Player/PlayerMovement.cs
+++ Game 2 - Untitled/Assets/Player/PlayerMovement.cs
@@ -4,11 +4,14 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
-    public float moveSpeed = 5f;
-    public float jumpForce = 1f;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 1f;
+    [SerializeField] private Animator _animator;
 
     float jump;
     float horizontalMovement;
+    bool isGrounded = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -19,23 +22,49 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         rb.linearVelocity = new Vector2(horizontalMovement * moveSpeed, rb.linearVelocity.y);
-        
+
     }
 
     public void Move(InputAction.CallbackContext context)
     {
         horizontalMovement = context.ReadValue<Vector2>().x;
+
+        if (context.ReadValue<Vector2>().x == 1)
+        {
+            transform.localScale = new Vector2(1, 1);
+        }
+        else if (context.ReadValue<Vector2>().x == -1)
+        {
+            transform.localScale = new Vector2(-1, 1);
+        }
+
+        if (context.ReadValue<Vector2>().x != 0)
+        {
+            _animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            _animator.SetBool("isWalking", false);
+        }
     }
 
     public void OnJump(InputAction.CallbackContext context)
     {
         Debug.Log($"Jumping {context.performed}");
         {
-            if (context.performed)
+            if (context.performed && isGrounded)
             {
                 rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+                isGrounded = false;
+                _animator.SetBool("isJumping", true);
             }
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isGrounded = true;
+        _animator.SetBool("isJumping", false);
     }
 
 }
