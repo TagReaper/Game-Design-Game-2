89bc4caeb18d3c8ea07dc357d570ccf53407b4f3
Shadow-nm <Maitland.Nathaniel@gmail.com>
2026-03-14 17:45:16 -0500

    Adds patrolling and attacking animations
    The enemy now patrols and area based on a left and right boundary. They will follow the player once they enter the player awareness radius and will attack the player once they reach the attack radius. Currently there are two bugs with the enemy. Once they leave the patrol area they try to return but stop at the nearest boundary and when they are on attack cooldown they freeze while still playing the walking animation. This will be fixed very soon.

--- Game 2 - Untitled/Assets/EnemyAI.cs
+++ Game 2 - Untitled/Assets/EnemyAI.cs
@@ -1,48 +1,83 @@
+using System;
+using System.Threading;
+using TMPro;
 using UnityEditor.Experimental.GraphView;
+using UnityEditor.SearchService;
+using UnityEditor.Tilemaps;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class EnemyAI : MonoBehaviour
 {
-    public GameObject player;
+    #region Variables
+    public Transform target;
+    public Transform leftLimit;
+    public Transform rightLimit;
+
     [SerializeField] private float _speed;
-    [SerializeField] private float _rotationSpeed;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private float attackDist;
+    [SerializeField] private float attackCD;
 
     private Rigidbody2D _rigidbody;
     private PlayerAwarenessScript _playerAwarenessController;
     private Vector2 _targetDirection;
-    
-    public float AggroDist;
-    public float AggroKeepDist;
-    public float rotationSpeed = 50f;
+    private float initialCD;
+    private bool cooling;
+    private bool attackMode;
+    #endregion
 
-    private float distance;
-    
+    #region Main Functions
     void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         _playerAwarenessController = GetComponent<PlayerAwarenessScript>();
+        initialCD = attackCD;
+        SelectTarget();
     }
 
     // Update is called once per frame
     private void FixedUpdate()
     {
-        UpdateTargetDirection();
-        RotateTowardsTarget();
-        SetVelocity();
-        //distance = Vector2.Distance(transform.position, player.transform.position);
-        //Vector2 direction = player.transform.position - transform.position;
+        if (!attackMode)
+        {
+            UpdateTargetDirection();
+            RotateTowardsTarget();
+            SetVelocity();
+        }
 
-        //transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        if(!InsideofLimits() && !_playerAwarenessController.AwareOfPlayer && !_animator.GetCurrentAnimatorStateInfo(0).IsName("enemyBODAttack"))
+        {
+            SelectTarget();
+        }
+
+        if (cooling)
+        {
+            Cooldown();
+            _animator.SetBool("isAttacking", false);
+            _animator.SetBool("canMove", true);
+        }
+        else
+        {
+            if (target != leftLimit && target !=rightLimit)
+            {
+                Attack();
+            }
+        }
     }
+    #endregion
+
+    #region Enemy Movement
     private void UpdateTargetDirection()
     {
         if (_playerAwarenessController.AwareOfPlayer)
         {
             _targetDirection = _playerAwarenessController.DirectionToPlayer;
+            target = FindAnyObjectByType<PlayerMovement>().transform;
         }
         else
         {
-            _targetDirection = Vector2.zero;
+            _targetDirection = target.position - this.transform.position;
         }
     }
 
@@ -52,32 +87,96 @@ public class EnemyAI : MonoBehaviour
         {
             return;
         }
-        else if (player.transform.position.x - transform.position.x > 0)
+        else if (target.transform.position.x - transform.position.x > 0)
         {
-            //transform.Rotate(0, 180, 0, Space.Self);
-            //transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
             transform.rotation = Quaternion.Euler(0, 180, 0);
-            //transform.localScale = new Vector2(-1, 1);
         }
-        else if (player.transform.position.x - transform.position.x < 0)
+        else if (target.transform.position.x - transform.position.x < 0)
         {
-            //transform.Rotate(0, 0, 0, Space.Self);
-            //transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
             transform.rotation = Quaternion.Euler(0, 0, 0);
-            //transform.localScale = new Vector2(1, 1);
         }
 
     }
 
     private void SetVelocity()
     {
-        if (_targetDirection == Vector2.zero)
+        if (!_animator.GetCurrentAnimatorStateInfo(0).IsName("enemyBODAttack"))
         {
+            if (_targetDirection == Vector2.zero)
+            {
+                _rigidbody.linearVelocity = Vector2.zero;
+                _animator.SetBool("isWalking", false);
+            }
+            else
+            {
+                Vector2 targetPositionXOnly = new Vector2(target.position.x, this.transform.position.y);
+                transform.position = Vector2.MoveTowards(this.transform.position, targetPositionXOnly, _speed * Time.deltaTime);
+                _animator.SetBool("isWalking", true);
+            }
+        }
+    }
+    #endregion
+
+    #region Enemy Attack
+    private void Attack()
+    {
+        if (Math.Abs(target.transform.position.x - transform.position.x) > attackDist)
+        {
+            cooling = false;
+            attackMode = false;
+            _animator.SetBool("isAttacking", false);
+            _animator.SetBool("canMove", true);
+        }
+        else if (Math.Abs(target.transform.position.x - transform.position.x) <= attackDist)
+        {
+            attackMode = true;
             _rigidbody.linearVelocity = Vector2.zero;
+            _animator.SetBool("canMove", false);
+            _animator.SetBool("isAttacking", true);
+            
+        }
+    }
+
+    private void Cooldown()
+    {
+        attackCD -= Time.deltaTime;
+
+        if (attackCD <= 0 && cooling && attackMode)
+        {
+            cooling = false;
+            attackCD = initialCD;
+        }
+    }
+
+    public void TriggerCooling()
+    {
+        cooling = true;
+    }
+    #endregion
+
+    #region Patrolling
+    private bool InsideofLimits()
+    {
+        return transform.position.x > leftLimit.position.x && transform.position.x < rightLimit.position.x;
+    }
+
+    private void SelectTarget()
+    {
+        float distanceToLeft = Vector2.Distance(transform.position, leftLimit.position);
+        float distanceToRight = Vector2.Distance(transform.position, rightLimit.position);
+
+        if(distanceToLeft > distanceToRight)
+        {
+            target = leftLimit;
         }
         else
         {
-            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, _speed * Time.deltaTime);
+            target = rightLimit;
         }
+
+        UpdateTargetDirection();
+        RotateTowardsTarget();
+        SetVelocity();
     }
+    #endregion
 }
